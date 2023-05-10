99391caa111c5be7a2db8657614c263d33be0a80
Andrey <agorevich@ecias.ru>
2023-05-09 22:48:11 -0600

    master: change password

--- keys/index.js
+++ keys/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   MONGODB_URI: 'mongodb+srv://dochorevych:4dc8pgzn8rifn5@node-with-mongodb.yneou6k.mongodb.net/shop',
   SESSION_SECRET: 'secret value',
-  SENDGRID_API_KEY: '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',
+  SENDGRID_API_KEY: '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',
   EMAIL_FROM: 'testnodejs@mail.com',
   BASE_URL: 'http://localhost:3001'
 }
--- routes/auth.js
+++ routes/auth.js
@@ -74,8 +74,14 @@ router.post('/register', async (req, res) => {
         email, name, password: hashPassword, cart: { items: [] }
       })
       await user.save();
-      await transporter.sendMail(regEmail(email));
-      res.redirect('/auth/login#login');
+      await transporter.sendMail(regEmail(email), (error, info) => {
+        if (error) {
+          console.log('Error in registration, email not sent: ',error);
+        } else {
+          res.redirect('/auth/login#login');
+          console.log('Email sent: ' + JSON.stringify(info));
+        }}
+      )
     }
   } catch(err) {
     console.log('Error in registration: ', err);
@@ -101,18 +107,73 @@ router.post('/reset', (req, res) => {
       const candidate = await User.findOne({email: req.body.email})
 
       if (candidate) {
-        candidate.resetToken = token
-        candidate.resetTokenExp = Date.now() + 60 * 60 * 1000
-        await candidate.save()
-        await transporter.sendMail(resetEmail(candidate.email, token))
-        res.redirect('/auth/login')
+        candidate.resetToken = token;
+        candidate.resetTokenExp = Date.now() + 60 * 60 * 1000;
+        await candidate.save();
+        await transporter.sendMail(resetEmail(candidate.email, token), (error, info) => {
+          if (error) {
+            console.log('Error in reset, email not reset: ',error);
+          } else {
+            console.log('Email reset: ' + JSON.stringify(info));
+          }}
+        )
+        res.redirect('/auth/login');
       } else {
         req.flash('error', 'There is no such email')
         res.redirect('/auth/reset')
       }
     })
   } catch (e) {
-    console.log(e)
+    console.log("Error: ",e)
+  }
+})
+
+router.get('/password/:token', async (req, res) => {
+  if (!req.params.token) {
+    return res.redirect('/auth/login');
+  }
+
+  try {
+    const user = await User.findOne({
+      resetToken: req.params.token,
+      resetTokenExp: {$gt: Date.now()}
+    })
+
+    if (!user) {
+      return res.redirect('/auth/login');
+    } else {
+      res.render('auth/password', {
+        title: 'Restore access',
+        error: req.flash('error'),
+        userId: user._id.toString(),
+        token: req.params.token
+      })
+    }
+  } catch(error) {
+    console.log('Error: ', error);
+  }
+})
+
+router.post('/password', async (req, res) => {
+  try {
+    const user = await User.findOne({
+      _id: req.body.userId,
+      resetToken: req.body.token,
+      resetTokenExp: {$gt: Date.now()}
+    })
+
+    if (user) {
+      user.password = await bcrypt.hash(req.body.password, 10);
+      user.resetToken = undefined;
+      user.resetTokenExp = undefined;
+      await user.save();
+      res.redirect('/auth/login');
+    } else {
+      req.flash('loginError', 'Life cycle token ended');
+      res.redirect('/auth/login');
+    }
+  } catch (e) {
+    console.log("Error: ",e)
   }
 })
 module.exports = router;
